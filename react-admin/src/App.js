a114cc9661def4a350dbf1532a8b113d808160d6
Anastasiia Onyshchenko <110947014+Anastasiia-softdev@users.noreply.github.com>
2022-10-16 21:40:45 -0400

    Update App.js

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -6,7 +6,6 @@ import Views from './views';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import { THEME_CONFIG } from './configs/AppConfig';
-
 const themes = {
   dark: `${process.env.PUBLIC_URL}/css/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/css/light-theme.css`,
